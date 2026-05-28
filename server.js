a9762817155b3f40e0c7afc6f2ebaccfa7cee090
mahakalpremier-max <mahakalpremier@gmail.com>
2026-05-28 23:15:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.post("/webhook", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
