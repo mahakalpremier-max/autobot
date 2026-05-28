04218af2069a25c7a8cd9dd24b1b412cef76e74e
mahakalpremier-max <mahakalpremier@gmail.com>
2026-05-28 22:53:25 +0530

    Update server.js
    fixed webhook

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Bot Running");
+});
+
 app.get("/webhook", (req, res) => {
 
   const verify_token = "mahakal123";
@@ -10,15 +14,20 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token === verify_token) {
+  if (mode === "subscribe" && token === verify_token) {
     return res.status(200).send(challenge);
   }
 
-  return res.sendStatus(403);
+  res.sendStatus(403);
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Bot running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
